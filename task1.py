515bdbe33a864fd60eadc1fbaef7c19824cd8a42
Ankityadav000-ux <ANKITTHOI000@GMAIL.COM>
2025-05-03 18:10:09 +0530

    Add files via upload

--- task1.py
+++ task1.py
@@ -30,7 +30,7 @@ def calculate_glasses_of_milk():
     height = float(input("Enter the height of the milk tank "))
     width = float(input("Enter the width of the milk tank"))
     breadth = float(input("Enter the breadth of the milk tank"))
-
+    
     glass_capacity = float(input("Enter the capacity of one glass"))
 
     tank_volume = height * width * breadth
@@ -84,3 +84,42 @@ if result:
     print(f"Number of dogs: {dogs}")
 else:
     print("No valid solution exists with the given heads and legs.")
+###7
+def calculate_sum_of_squares(n):
+    sum_of_squares = 0
+    for i in range(1, n + 1):
+        sum_of_squares += i ** 2
+    return sum_of_squares
+
+n = int(input("Enter the value of n: "))
+
+print("The sum of squares of first", n, "natural numbers is:", calculate_sum_of_squares(n))
+###8
+def calculate_simple_interest(principal, rate, time):
+    return (principal * rate * time) / 100
+
+principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the rate of interest (per annum): "))
+time = float(input("Enter the time period (in years): "))
+
+simple_interest = calculate_simple_interest(principal, rate, time)
+
+
+print(f"The Simple Interest is: {simple_interest}")
+
+###9
+def find_nth_term(a1, a2, n):
+    d = a2 - a1  
+    return a1 + (n - 1) * d
+
+def main():
+    # Get input from the user
+    a1 = float(input("Enter the first term (a1): "))
+    a2 = float(input("Enter the second term (a2): "))
+    n = int(input("Enter the term number (n): "))
+    
+    an = find_nth_term(a1, a2, n)
+    print(f"The {n}th term of the arithmetic series is: {an}")
+
+main()
+
